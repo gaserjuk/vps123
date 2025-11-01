aaf21b9b8832aecca5ec0efed987f9a03cc4854c
andrygibhq00 <133579346+andrygibhq00@users.noreply.github.com>
2025-11-01 11:52:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt update -y
 
 echo "=== Installing Xfce4 ==="
-sudo apt install xfce4 xrdp -y > /dev/null 2>&1
+sudo apt install xfce4 xrdp -y
 
 echo "startxfce4" > ~/.xsession
 sudo chown $(whoami):$(whoami) ~/.xsession > /dev/null 2>&1

e9e1929d5c7eaa6de1bd0c17167fde2373ed0a46
andrygibhq00 <133579346+andrygibhq00@users.noreply.github.com>
2025-11-11 13:51:00 +0700

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -7,20 +7,22 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb 
 sudo dpkg -i chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
 sudo apt --fix-broken install -y > /dev/null 2>&1
 
-echo "===> Installing Xfce4"
-sudo apt install xfce4 xfce4-goodies -y > /dev/null 2>&1
-echo "exec xfce4-session" > ~/.chrome-remote-desktop-session
 
-echo "===> Installing Gdown"
+echo "===> Installing Xfce"
+sudo apt install xfce4 xfce4-goodies -y > /dev/null 2>&1
+sudo apt remove gnome-terminal -y > /dev/null 2>&1
+sudo apt install remmina remmina-plugin-rdp remmina-plugin-vnc remmina-plugin-secret -y > /dev/null 2>&1
+
+echo "exec xfce4-session" > ~/.chrome-remote-desktop-session
+chmod +x ~/.chrome-remote-desktop-session
+
+
+echo "===> Installing Chrome"
 sudo apt install python3-pip -y > /dev/null 2>&1
 sudo pip install gdown > /dev/null 2>&1
 
-echo "===> Installing Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 sudo dpkg -i google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 sudo apt --fix-broken install -y > /dev/null 2>&1
 
-echo "===> Installing Remmina"
-sudo snap install remmina > /dev/null 2>&1
-
-chmod +x ~/.chrome-remote-desktop-session
+echo "https://remotedesktop.google.com/headless"
